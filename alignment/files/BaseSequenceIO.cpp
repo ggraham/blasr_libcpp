24bb1a93a9dbc900eed21a6cc7357d8670e62f32
Yuan Li <yli@pacificbiosciences.com>
2015-12-16 14:19:50 -0800

    enum class FileType, subsequent change of 17dbd6d

--- alignment/files/BaseSequenceIO.cpp
+++ alignment/files/BaseSequenceIO.cpp
@@ -25,50 +25,50 @@ int BaseSequenceIO::DetermineFileTypeByExtension(string &fileName,
                 extension == "fsta" or
                 extension == "screen"
            ) {
-            type = Fasta;
+            type = FileType::Fasta;
             return 1;
         }
         else if (extension == "h5") {
             dotPos = fileName.rfind(".", dotPos-1); 
             extension.assign(fileName, dotPos+1, fileName.size() - (dotPos + 1));
             if (extension == "pls.h5" or extension == "plx.h5" ) {
-                type = HDFPulse;
+                type = FileType::HDFPulse;
                 return 1;
             }
             else if (extension == "bas.h5" or extension == "bax.h5") {
-                type = HDFBase;
+                type = FileType::HDFBase;
                 return 1;
             }
             else if (extension == "ccs.h5") {
-                type = HDFCCSONLY;
+                type = FileType::HDFCCSONLY;
                 return 1;
             }
             else {
-                type = None;
+                type = FileType::None;
                 return 0;
             }
         }
         else if (extension == "fastq" or
                 extension == "fq") {
-            type = Fastq;
+            type = FileType::Fastq;
             return 1;
         }
         else if (extension == "4bit" or
                 extension == "fourbit") {
-            type = Fourbit;
+            type = FileType::Fourbit;
             assert("Four bit reading is not yet implemented for the reader agglomerate!" == 0);
             return 1;
         }
         else if (extension == "bam") {
-            type = PBBAM;
+            type = FileType::PBBAM;
             return 1;
         }
         else if (extension == "xml") {
-            type = PBDATASET;
+            type = FileType::PBDATASET;
             return 1;
         }
         else {
-            type = None;
+            type = FileType::None;
             if (exitOnFailure) {
                 cout << "ERROR, file type '." << extension << "' is not understood to be one of pls.h5, fasta, fastq, nor bam. " << endl;
                 exit(1);
